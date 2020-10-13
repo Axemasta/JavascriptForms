97ff37587bd6e90c3c0c81d3d7bc8638636f0cae
Axemasta <33064621+Axemasta@users.noreply.github.com>
2020-10-13 13:02:50 +0100

    Added Basic Invoke Name iOS
    The javascript event for the hello name is being invoked in the xamarin app, no fancy styling just yet

--- src/JavascriptForms/Pages/InvokeNamePage.xaml.cs
+++ src/JavascriptForms/Pages/InvokeNamePage.xaml.cs
@@ -10,6 +10,18 @@ namespace JavascriptForms.Pages
         public InvokeNamePage()
         {
             InitializeComponent();
+
+            this.BindingContextChanged += InvokeNamePage_BindingContextChanged;            
+        }
+
+        private void InvokeNamePage_BindingContextChanged(object sender, EventArgs e)
+        {
+            var vm = ((ViewModels.InvokeNameViewModel)this.BindingContext);
+
+            if (vm == null)
+                return;
+
+            HybridWebView.RegisterAction(data => vm.InvokeNameCommand.Execute(data));
         }
     }
 }
--- src/JavascriptForms/ViewModels/InvokeNameViewModel.cs
+++ src/JavascriptForms/ViewModels/InvokeNameViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using Prism.Commands;
+using Prism.Services;
+
 namespace JavascriptForms.ViewModels
 {
     public class InvokeNameViewModel : ViewModelBase
     {
-        public InvokeNameViewModel()
+        private readonly IPageDialogService _dialogService;
+
+        public DelegateCommand<string> InvokeNameCommand { get; private set; }
+
+        public InvokeNameViewModel(IPageDialogService dialogService)
         {
+            _dialogService = dialogService;
+
             Title = "Invoke Name";
+
+            InvokeNameCommand = new DelegateCommand<string>(OnInvokeName);
+        }
+
+        public async void OnInvokeName(string name)
+        {
+            await _dialogService.DisplayAlertAsync($"Hello {name}", "If you are reading your name, the javascript invokation was successful!", "OK");
         }
     }
 }
