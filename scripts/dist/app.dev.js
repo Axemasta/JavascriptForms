d5a3adb2435a62dd05e03a2f50a0310311b2507b
Axemasta <33064621+Axemasta@users.noreply.github.com>
2020-10-13 11:07:32 +0100

    Added Basic Website

---
+++ scripts/dist/app.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// {/* <script type="text/javascript">function log(str) {
+//     $('#result').text($('#result').text() + " " + str);
+// }
+// function invokeCSCode(data) {
+//     try {
+//         log("Sending Data:" + data);
+//         invokeCSharpAction(data);
+//     }
+//     catch (err) {
+//         log(err);
+//     }
+// }
+// //onclick="javascript: invokeCSCode($('#name').val());"
+// </script> */}
+$(document).ready(function () {});
